e87ed3638a033229d5497881e60ffa7044ec5de5
cg3831204 <cg3831204@gmail.com>
2022-04-23 04:31:47 +1000

    Better naming clarity in main

--- main.rb
+++ main.rb
@@ -5,20 +5,20 @@ require_relative "./lib/product.rb"
 
 class Main
   def self.run
-    catalog_a = Source::CatalogItem.from_raw_data("a")
-    barcodes_a = Source::Barcode.from_raw_data("a")
-    catalog_b = Source::CatalogItem.from_raw_data("b")
-    barcodes_b = Source::Barcode.from_raw_data("b")
+    source_catalog_a = Source::CatalogItem.from_raw_data("a")
+    source_barcodes_a = Source::Barcode.from_raw_data("a")
+    source_catalog_b = Source::CatalogItem.from_raw_data("b")
+    source_barcodes_b = Source::Barcode.from_raw_data("b")
 
     products_a = Product.load_products_from_source(
-      source_catalog: catalog_a,
-      source_barcodes: barcodes_a,
+      source_catalog: source_catalog_a,
+      source_barcodes: source_barcodes_a,
       source_name: "A",
     )
 
     products_b = Product.load_products_from_source(
-      source_catalog: catalog_b,
-      source_barcodes: barcodes_b,
+      source_catalog: source_catalog_b,
+      source_barcodes: source_barcodes_b,
       source_name: "B",
     )
 
